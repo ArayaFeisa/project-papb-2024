bc0c77cd572e5e8c8039b89f22905fcc44acbcb9
fairanabilaa <150148469+fairanabilaa@users.noreply.github.com>
2024-03-12 20:20:06 +0700

    DetailActivity.java

--- app/src/main/java/com/example/kioskita/DetailActivity.java
+++ app/src/main/java/com/example/kioskita/DetailActivity.java
@@ -1,1 +1,31 @@
+package com.example.cobakioskita.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cobakioskita.R;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        TextView textView = findViewById(R.id.textView3);
+        String kelas = getIntent().getStringExtra("kelas");
+        textView.setText(kelas);
+    }
+}
+
 
