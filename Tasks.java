3386d698805b6ecf3a1799f5fb2658d17b728d61
ArayaFeisa <128389876+ArayaFeisa@users.noreply.github.com>
2024-05-13 18:09:14 +0700

    test

---
+++ Tasks.java
@@ -0,0 +1,14 @@
+package com.example.kioskitalocal;
+
+import android.graphics.Bitmap;
+
+public class Tasks {
+    public String judul;
+    public String desc;
+    public Bitmap foto;
+
+    public Tasks(String judul, String desc){
+        this.judul = judul;
+        this.desc = desc;
+    }
+}
